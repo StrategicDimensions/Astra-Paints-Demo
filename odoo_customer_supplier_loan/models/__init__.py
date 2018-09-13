61253aa8c95a3f54ad57f0e76f4846a6fb3037d5
Daniel Maimba <daniel@strategicdimensions.co.za>
2018-09-13 06:27:04 +0200

    [ADD] Customer Supplier Loans

---
+++ odoo_customer_supplier_loan/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import loan_payroll
+#import prepayment_writeoff
+from . import payment
+from . import loan_proof
+from . import loan_installment_journal
+# import hr_salary_rule #todo check
+# import hr_payslip #todo check
